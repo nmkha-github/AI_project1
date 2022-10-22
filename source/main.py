a4c54effaa3b8f4caf8927b7d54f391a4f3897b6
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2022-10-22 16:32:49 +0700

    Add BFS

--- source/main.py
+++ source/main.py
@@ -1,16 +1,17 @@
 from lib import *
 from search_algorithm.dfs import *
+from search_algorithm.bfs import *
 
 from cell import Cell
 
 if __name__ == "__main__":
     bonus_points,  cellMatrix, beginCell, exitCell = read_file(
-        'input/level_1/input1.txt')
+        '../input/level_1/input1.txt')
 
     makeAdjList(cellMatrix)
     visitedOrder = []
 
-    dfs(beginCell, exitCell, visitedOrder)
+    bfs(beginCell, exitCell, visitedOrder)
     route = make_route(cellMatrix, beginCell, exitCell)
 
     # print(visitedOrder)
