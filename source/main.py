e749ad39828d3eb6a55e71a660386b60a7370502
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2022-10-23 12:56:47 +0700

    Add extend BFS function

--- source/main.py
+++ source/main.py
@@ -3,6 +3,7 @@ from search_algorithm.Heuristic import *
 from search_algorithm.dfs import *
 from search_algorithm.ucs import *
 from search_algorithm.bfs import *
+from search_algorithm.extendbfs import *
 from search_algorithm.astar import *
 from search_algorithm.gbfs import *
 from search_algorithm.Heuristic import *
@@ -10,20 +11,21 @@ import os
 from cell import *
 
 if __name__ == "__main__":
-    for filename in os.listdir('input/level_1/'):
-        path = os.path.join('input/level_1/', filename)
+    for filename in os.listdir('input/level_3/'):
+        path = os.path.join('input/level_3/', filename)
         filenameTrip = filename.rsplit('.', 1)[0]
-        os.makedirs(f'output/level_1/{filenameTrip}/')
-        bonus_points,  cellMatrix, beginCell, exitCell = read_file(path)
+        os.makedirs(f'output/level_3/{filenameTrip}/')
+        bonus_points,  cellMatrix, beginCell, exitCell = read_file_advance(path)
         makeAdjList(cellMatrix)
         visitedOrder = []
         heuristicA = Heuristic(exitCell, 1)
-        gbfs(beginCell, exitCell, visitedOrder, heuristicA)
+        'gbfs(beginCell, exitCell, visitedOrder, heuristicA)'
+        extendbfs(beginCell,exitCell,visitedOrder)
         route = make_route(cellMatrix, beginCell, exitCell)
-        out = f'output/level_1/{filenameTrip}/gbfs.jpg'
+        out = f'output/level_3/{filenameTrip}/extendbfs.jpg'
         visualize_maze(cellMatrix, bonus_points, [beginCell.row, beginCell.col], [
                        exitCell.row, exitCell.col], route=route, outputPath=out)
-        f = open(f'output/level_1/{filenameTrip}/gbfs.txt', 'w')
+        f = open(f'output/level_3/{filenameTrip}/extendbfs.txt', 'w')
         if exitCell.prev == None:
             f.write("NO")
         else:
